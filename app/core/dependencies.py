17413150e7cfd3c5ffaa5ce8881b475e0c866251
Artem K. <artem.kabatov22@gmail.com>
2026-05-13 11:11:14 +0300

    feat: add employee service dependency

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -5,9 +5,16 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.database import db
 from app.services.department import DepartmentService
+from app.services.employee import EmployeeService
 
 
 async def get_department_service(
     db: AsyncSession = Depends(db.session_dependency),
 ) -> DepartmentService:
     return DepartmentService(db)
+
+
+async def get_employee_service(
+    db: AsyncSession = Depends(db.session_dependency),
+) -> EmployeeService:
+    return EmployeeService(db)

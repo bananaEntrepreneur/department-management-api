f4197757f2926347c7be7793ebcf2e67bb541445
Artem K. <artem.kabatov22@gmail.com>
2026-05-13 10:56:38 +0300

    feat: add POST /departments/{id}/employees/ endpoint

--- app/api/routes/department.py
+++ app/api/routes/department.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from starlette import status
 
-from app.core.dependencies import get_department_service
+from app.core.dependencies import get_department_service, get_employee_service
+from app.schemas.employee import AddEmployeeRequest, AddEmployeeResponse, EmployeeDTO
 from app.schemas.department import DepartmentCreate, DepartmentDTO
 from app.services.department import DepartmentService
+from app.services.employee import EmployeeService
 
 departments_router = APIRouter(prefix="/departments", tags=["Departments"])
 
@@ -23,3 +25,25 @@ async def add_department(
         parent_id=request.parent_id,
     )
     return DepartmentDTO.model_validate(department)
+
+
+@departments_router.post(
+    path="/{id}/employees/",
+    response_model=AddEmployeeResponse,
+    status_code=status.HTTP_201_CREATED,
+    summary="Add employee in department",
+)
+async def add_employee(
+        id: Query(..., ge=1, description="Department ID"),
+        request: AddEmployeeRequest,
+        employee_service: EmployeeService = Depends(get_employee_service),
+) -> AddEmployeeResponse:
+    employee = await employee_service.add_employee(
+        department_id=id,
+        full_name=request.full_name,
+        position=request.position,
+        hired_at=request.hired_at,
+    )
+    return AddEmployeeResponse(
+        employee=EmployeeDTO.model_validate(employee),
+    )
